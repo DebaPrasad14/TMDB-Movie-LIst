3464652d526fcd6c23f7148e9b94f34ac060e83e
debaprasad14 <debaprasad@eichiba.com>
2020-11-28 22:50:13 +0530

    added project src structure & routes

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <b-card class="mv-card">
+      <div class="text-center mb-4">
+        <h3>Find perfect movie for <b>evening</b></h3>
+      </div>
+      <b-input-group class="mv-search-group">
+        <b-input-group-prepend>
+          <b-input-group-text>
+            <b-icon icon="search" aria-hidden="true" font-scale="0.97"></b-icon>
+          </b-input-group-text>
+        </b-input-group-prepend>
+        <b-form-input
+          id="input-search-text"
+          class="mv-search-form"
+          v-model="searchItem"
+          trim
+          type="text"
+          placeholder="Search movies"
+          autocomplete="off"
+        ></b-form-input>
+        <b-button class="mv-button ml-2">SEARCH</b-button>
+      </b-input-group>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchItem: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.mv-card {
+  background: #f2f1f8;
+  border-radius: 0.5em;
+  padding: 3em;
+  border: 1px solid #dbe1e9;
+}
+.mv-search-group .input-group-text {
+  background: #fff;
+  border: 1px solid #dbe1e9;
+  border-right: none !important;
+}
+.mv-search-group .mv-search-form,
+.mv-search-group .mv-search-form:focus {
+  border: 1px solid #dbe1e9;
+  border-left: none;
+  border-radius: 0 0.25em 0.25em 0;
+  outline: 0 !important;
+  box-shadow: none !important;
+}
+.mv-button {
+  background: rgba(104, 85, 214, 0.54);
+  border: none;
+  border-radius: 0.35em;
+  padding: 0 2em;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  color: #fff;
+}
+.btn:active,
+.btn:focus,
+.btn:visited,
+.btn:hover {
+  background: rgba(104, 85, 214, 0.54) !important;
+  outline: 0 !important;
+  border: none;
+  box-shadow: none !important;
+}
+</style>
---
+++ src/views/MovieList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container mv-container">
+    <div class="mt-4">
+      <template v-if="isLoading">
+        <div class="ct-centered">
+          <Loader />
+        </div>
+      </template>
+      <template v-else-if="isError">
+        <div class="ct-centered">
+          <PageError />
+        </div>
+      </template>
+      <template v-else>
+        <SearchForm />
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchForm from "@/components/SearchForm";
+import PageError from "@/components/PageError";
+import Loader from "@/components/Loader";
+// import axios from "axios";
+
+export default {
+  components: {
+    SearchForm,
+    PageError,
+    Loader,
+  },
+  data() {
+    return {
+      isLoading: false,
+      isError: false,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap");
+.mv-container {
+  font-family: "Inter", sans-serif;
+}
+</style>
